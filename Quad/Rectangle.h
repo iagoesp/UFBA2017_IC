9418f67fe5fd00e44eaf798d96d9cdb796b604a5
iagoesp <32596872+iagoesp@users.noreply.github.com>
2017-11-29 08:33:21 -0300

    Implementação Quad

---
+++ Quad/Rectangle.h
@@ -0,0 +1,40 @@
+#ifndef Rectangle_H
+#define Rectangle_H
+
+#include "Shader.h"
+#include "Utility.h"
+
+namespace byhj
+{
+
+class Rectangle
+{
+
+public:
+	Rectangle()  {}
+	~Rectangle() {}
+
+public:
+
+	void Init()		;
+	void Render()	;
+	void Shutdown()	;
+
+
+private:
+	void init_shader();
+	void init_buffer();
+	void init_vertexArray();
+
+
+private:
+
+	GLuint vao = byhj::OGL_VALUE;
+	GLuint vbo = byhj::OGL_VALUE;
+	GLuint program = byhj::OGL_VALUE;
+	ogl::Shader RectangleShader = { "Rectangle Shader" };
+};
+
+
+}
+#endif
