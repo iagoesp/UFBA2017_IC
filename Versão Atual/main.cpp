d9721a2108fb6dcfd7f030f6922835a92a77fe94
iagoesp <32596872+iagoesp@users.noreply.github.com>
2018-08-28 20:33:56 -0300

    atualizando

--- Versão Atual/main.cpp
+++ Versão Atual/main.cpp
@@ -8,22 +8,17 @@
 
 // Include GLFW
 #include <GLFW/glfw3.h>
+GLFWwindow* window;
 
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include "glm/ext.hpp"
-
 using namespace glm;
-
 using namespace std;
 
 #include "LoadShaders.hpp"
 #include "controls.hpp"
-
 extern glm::vec3 position;
-
-GLFWwindow* window;
 
 static GLsizei IndexCount;
 static const GLuint PositionSlot = 0;
@@ -34,7 +29,7 @@ int main(int argv, char** argc){
 	// Initialise GLFW
     glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
@@ -74,7 +69,7 @@ int main(int argv, char** argc){
 	//glDepthFunc(GL_LESS);
 
 	// Cull triangles which normal is not towards the camera
-	glEnable(GL_CULL_FACE);
+	//glEnable(GL_CULL_FACE);
 //	if()
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -83,27 +78,24 @@ int main(int argv, char** argc){
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-    //GLuint programID = LoadShaders( "Geodesic.vert",  "Geodesic.tesc", "Geodesic.tese", "Geodesic.frag");
-	GLuint programID = LoadShaders( "Geodesic.vert", "Geodesic.geom", "Geodesic.frag");
-	//GLuint programID = LoadShaders( "Geodesic.vert", "Geodesic.frag");
+    //GLuint programID = LoadShaders( "tessAdapt.vert",  "tessAdapt.tesc", "tessAdapt.tese", "tessAdapt.frag");
+    GLuint programID = LoadShaders( "tessUnif.vert",  "tessUnif.tesc", "tessUnif.tese", "tessUnif.frag");
+//	GLuint programID = LoadShaders( "Geodesic.Vertex", "Geodesic.Fragment");
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID             = glGetUniformLocation(programID, "MVP");
 	GLuint ModelMatrixID        = glGetUniformLocation(programID, "M");
 	GLuint ViewMatrixID         = glGetUniformLocation(programID, "V");
 	GLuint ProjectionMatrixID   = glGetUniformLocation(programID, "P");
-	GLuint NormalMatrixID       = glGetUniformLocation(programID, "N");
     GLuint TessLevelInnerID     = glGetUniformLocation(programID, "TessLevelInner" );// Inner tessellation paramter
     GLuint TessLevelOuterID     = glGetUniformLocation(programID, "TessLevelOuter" );  // TessLevelOuter tessellation paramter
-    GLuint distanceID           = glGetUniformLocation(programID, "distance");
-    GLuint cameraPosIDX         = glGetUniformLocation(programID, "px");
-    GLuint cameraPosIDY         = glGetUniformLocation(programID, "py");
-    GLuint cameraPosIDZ         = glGetUniformLocation(programID, "pz");
+    GLuint cameraPosIDX          = glGetUniformLocation(programID, "px");
+    GLuint cameraPosIDY          = glGetUniformLocation(programID, "py");
+    GLuint cameraPosIDZ          = glGetUniformLocation(programID, "pz");
 	GLuint ampValue             = glGetUniformLocation(programID, "amp");
-	//GLuint planeID              = glGetUniformLocation(programID, "planes");
 
     vector<unsigned short> indices;
-    const GLuint index = 40;
+    const GLuint index = 40.0;
     const GLfloat meshSize = 40.0;
     float tamAmostra = meshSize / (float)index;
     for (GLuint i = 0 ; i < index ; i++){
@@ -118,12 +110,22 @@ int main(int argv, char** argc){
 		}
 	}
 
-    float amp = 4.0;
+    float freq = 3.0, amp = 4.0;
     vector<glm::vec3> vec_y;
     vector<GLfloat> vertices;
     float minn=100000000, maxx=-1000000000;
     for (GLfloat i = 0 ; i <= index ; i+=1.0){
 		for (GLfloat j = 0 ; j <= index ; j+=1.0) {
+            float y = cos((i*tamAmostra)) * sin((j*tamAmostra));
+            y = y*amp;
+            //cout<<"y = "<< y<<endl;
+            if(y<minn) minn=y;
+            if(y>maxx) maxx=y;
+            glm::vec3 aux;
+            aux.x = i*tamAmostra;
+            aux.y = y;
+            aux.z = j*tamAmostra;
+            vec_y.push_back(aux);
             vertices.push_back((float)(i*tamAmostra));
             vertices.push_back((float)(j*tamAmostra));
         }
@@ -139,21 +141,24 @@ int main(int argv, char** argc){
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
 
-
     // Create the VBO for indices:
     GLuint elementbuffer;
     glGenBuffers(1, &elementbuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), &indices[0], GL_STATIC_DRAW);
 
-    GLuint normalbuffer;
-    glGenBuffers(1, &normalbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+    //GLuint normalbuffer;
+    //glGenBuffers(1, &normalbuffer);
+    //glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+//	glBufferData(GL_ARRAY_BUFFER, indexed_normals.size() * sizeof(glm::vec3), &indexed_normals[0], GL_STATIC_DRAW);
+
 
     // For speed computation
     TessLevelInner = 1.0f;
-    TessLevelOuter = 1.0f;
+    TessLevelOuter = 4.0f;
+    float distance;
     glm::vec3 camerapos = position;
+    float minnn=100000000, maxxx=-1000000000;
 
     do{
         // Clear the screen
@@ -167,68 +172,44 @@ int main(int argv, char** argc){
         glm::mat4 ProjectionMatrix = getProjectionMatrix();
         glm::mat4 ViewMatrix = getViewMatrix();
         glm::mat4 ModelMatrix = glm::mat4(1.0);
-        glm::mat4 NormalMatrix = glm::inverseTranspose(ModelMatrix);
         glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-        //glm::mat4 proj_view = ProjectionMatrix * ViewMatrix;
+
+
 
         // Send our transformation to the currently bound shader,
         // in the "MVP" uniform
         float px = position.x; float py = position.y; float pz = position.z;
-        float vecx = 20.0; float vecy = (minn+maxx)/2; float vecz = 20.0;
-        float d = sqrt(pow( (float)(px - vecx) ,2) +
-                       pow( (float)(py - vecy) ,2) +
-                       pow( (float)(pz - vecz) ,2));
-
-        if (d<=20)              TessLevelInner = 32;
-        else if (d<=30 && d>20) TessLevelInner = 16;
-        else if(d<=40 && d>30)  TessLevelInner = 8;
-        else if(d<=50 && d>40)  TessLevelInner = 4;
-        else if(d<=60 && d>50)  TessLevelInner = 2;
-        else if(d>60)           TessLevelInner = 1;
-        TessLevelOuter = TessLevelInner;
-
+               //cout<<"     min = "<<minnn<<" e max = "<<maxxx<<endl;
         if (glfwGetKey( window, GLFW_KEY_L ) == GLFW_PRESS){
            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         }
         if (glfwGetKey( window, GLFW_KEY_F ) == GLFW_PRESS){
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
         }
+
+        if (glfwGetKey( window, GLFW_KEY_Y ) == GLFW_PRESS){
+            glEnable(GL_CULL_FACE);
+        }
+
         if (glfwGetKey( window, GLFW_KEY_U ) == GLFW_PRESS){
-           	glEnable(GL_CULL_FACE);
-
-        }
-        if (glfwGetKey( window, GLFW_KEY_I ) == GLFW_PRESS){
             glDisable(GL_CULL_FACE);
-
         }
-
-        /*if (glfwGetKey( window, GLFW_KEY_C ) == GLFW_PRESS){
-           TessLevelInner+=1.0;
-        }
-        if (glfwGetKey( window, GLFW_KEY_V ) == GLFW_PRESS){
-            TessLevelInner = TessLevelInner > 1 ? TessLevelInner - 1 : 1;
-        }
-        if (glfwGetKey( window, GLFW_KEY_B ) == GLFW_PRESS){
-            TessLevelOuter+=1.0;
-        }
-        if (glfwGetKey( window, GLFW_KEY_N ) == GLFW_PRESS){
-            TessLevelOuter = TessLevelOuter > 1 ? TessLevelOuter - 1 : 1;
-        }*/
 
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
         glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
         glUniformMatrix4fv(ProjectionMatrixID, 1, GL_FALSE, &ProjectionMatrix[0][0]);
-        glUniformMatrix4fv(NormalMatrixID, 1, GL_FALSE, &NormalMatrix[0][0]);
-        //glUniform4fv(planeID, 6, glm::value_ptr(plan[0]));
+        glUniform1f( TessLevelInnerID, TessLevelInner );
+        glUniform1f( TessLevelOuterID, TessLevelOuter );
         glUniform1f(cameraPosIDX, px);
         glUniform1f(cameraPosIDY, py);
         glUniform1f(cameraPosIDZ, pz);
         glUniform1f(ampValue,amp);
 
-        //glPatchParameteri(GL_PATCH_VERTICES, 3);
+
+
+        glPatchParameteri(GL_PATCH_VERTICES, 3);
 
         // 1rst attribute buffer : vertices
         glEnableVertexAttribArray(0);
@@ -239,7 +220,7 @@ int main(int argv, char** argc){
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
 
         // Draw the triangles !
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, (void*)0);
+        glDrawElements(GL_PATCHES, indices.size(), GL_UNSIGNED_SHORT, (void*)0);
 
         glDisableVertexAttribArray(0);
 
---
+++ Versão Atual/tessUnif.tese
@@ -0,0 +1,51 @@
+#version 430 core
+
+layout(triangles, equal_spacing, cw) in;
+
+//uniform vec4 planes[6];
+uniform mat4 P;
+uniform mat4 V;
+uniform mat4 MVP;
+
+in vec3 tcPosition[];
+in vec3 tcNormal[];
+in vec4 tcColor[];
+
+out float p;
+out vec3 vcNormal;
+out vec4 vcColor;
+
+out vec3 tePosition;
+
+void main(){
+    vec3 p0 = gl_TessCoord.x * tcPosition[0];
+    vec3 p1 = gl_TessCoord.y * tcPosition[1];
+    vec3 p2 = gl_TessCoord.z * tcPosition[2];
+    tePosition = (p0 + p1 + p2);
+
+    vec3 n0 = gl_TessCoord.x * tcNormal[0];
+    vec3 n1 = gl_TessCoord.y * tcNormal[1];
+    vec3 n2 = gl_TessCoord.z * tcNormal[2];
+    vcNormal = (n0 + n1 + n2);
+
+    vec4 c0 = gl_TessCoord.x * tcColor[0];
+    vec4 c1 = gl_TessCoord.y * tcColor[1];
+    vec4 c2 = gl_TessCoord.z * tcColor[2];
+    vcColor = (c0 + c1 + c2);
+
+    p = tePosition.y;
+    /*
+    bool point_inside = true;
+    for (int i = 0; i < 6; ++i){
+        if (planes[i].x * tePosition.x + planes[i].y * tePosition.y +
+            planes[i].z * tePosition.z + planes[i].w <= 0){
+                point_inside = true;
+        }
+        else{
+            point_inside = false;
+            break;
+        }
+    }
+    if(point_inside) */
+        gl_Position = MVP * vec4(tePosition, 1.0);
+}
